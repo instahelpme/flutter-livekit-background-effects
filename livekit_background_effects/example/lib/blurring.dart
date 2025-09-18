10b6b0a481ad54fa516267e76efea828b42b8fc4
Tobias Ollmann <tobias.ollmann@instahelp.me>
2025-09-18 16:53:02 +0200

    chore: finish example

--- livekit_background_effects/example/lib/blurring.dart
+++ livekit_background_effects/example/lib/blurring.dart
@@ -7,12 +7,71 @@
  */
 
 import 'package:flutter/material.dart';
+import 'package:livekit_background_effects/livekit_background_effects.dart';
+import 'package:livekit_components/livekit_components.dart';
 
-class Blurring extends StatelessWidget {
+class Blurring extends StatefulWidget {
   const Blurring({super.key});
 
   @override
+  State<Blurring> createState() => _BlurringState();
+}
+
+class _BlurringState extends State<Blurring> {
+  late final LivekitBackgroundEffects _processor;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _processor = LivekitBackgroundEffects();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Text("Hallo");
+    final roomContext = RoomContext.of(context)!;
+    void switchToBackground(bg) async {
+      final track = roomContext
+          .localParticipant
+          ?.videoTrackPublications
+          .firstOrNull
+          ?.track;
+      if (track?.processor != _processor) {
+        await track?.setProcessor(_processor);
+      }
+      if (!mounted) return;
+
+      await _processor.updateBackground(bg);
+    }
+
+    return Wrap(
+      children: [
+        FilledButton(
+          onPressed: () =>
+              switchToBackground(LivekitBackgroundEffectsOptions.none()),
+          child: Text("none"),
+        ),
+        FilledButton(
+          onPressed: () => switchToBackground(
+            LivekitBackgroundEffectsOptions.lightBlurring(),
+          ),
+          child: Text("light"),
+        ),
+        FilledButton(
+          onPressed: () => switchToBackground(
+            LivekitBackgroundEffectsOptions.lightBlurring(),
+          ),
+          child: Text("heavy"),
+        ),
+        FilledButton(
+          onPressed: () => switchToBackground(
+            LivekitBackgroundEffectsOptions.virtualBackground(
+              BackgroundPresets.mountains,
+            ),
+          ),
+          child: Text("outside"),
+        ),
+      ],
+    );
   }
 }
